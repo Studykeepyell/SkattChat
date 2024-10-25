ca7a09526dd47e695c0dfa2020fa5f026b413da5
Studykeepyell <140637608+Studykeepyell@users.noreply.github.com>
2024-10-24 21:22:28 -0400

    Demo

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '../public')));
 
 // MongoDB connection
-const mongoURI = process.env.MONGO_URI || 'fallback-mongo-connection-string';
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://Sky:Sky090726@cluster1.ripon.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1';
 mongoose.connect(mongoURI, {
     serverSelectionTimeoutMS: 30000 // 30 seconds timeout
 })
