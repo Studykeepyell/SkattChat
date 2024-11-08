a99a42363c45c960f0b9807d880a2c649364d6ea
Studykeepyell <140637608+Studykeepyell@users.noreply.github.com>
2024-11-08 10:32:46 -0500

    demo0.2

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,12 @@ app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Atlas connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/users', userRoutes);
